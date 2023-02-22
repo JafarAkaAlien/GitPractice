36f1a990fd3562ba11902384fbbee7e90a9e7ff7
Jafar Tarverdiyev <cefertarverdiyevv@gmail.com>
2023-02-22 10:37:59 +0400

    salam

--- app/src/main/java/com/leblebi/gitpractice/MainActivity.kt
+++ app/src/main/java/com/leblebi/gitpractice/MainActivity.kt
@@ -7,9 +7,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 <<<<<<< HEAD
 
+<<<<<<< HEAD
 =======
 >>>>>>> parent of 602884d (ikinci)
+=======
+    fun salam(){
+        println("salam")
+    }
+    aybla
+>>>>>>> parent of bfa125c (bilmirem)
 }
