1a1205e22a58883eba87a5f9761f59597e82bb3b
Jafar Tarverdiyev <cefertarverdiyevv@gmail.com>
2023-02-22 10:36:21 +0400

    bla

--- app/src/main/java/com/leblebi/gitpractice/MainActivity.kt
+++ app/src/main/java/com/leblebi/gitpractice/MainActivity.kt
@@ -8,5 +8,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+<<<<<<< HEAD
 
+=======
+>>>>>>> parent of 602884d (ikinci)
 }
