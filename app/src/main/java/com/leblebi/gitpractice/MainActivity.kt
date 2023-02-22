29635bdef36637a3c072567c1fd0bea114018d07
Jafar Tarverdiyev <cefertarverdiyevv@gmail.com>
2023-02-22 10:20:30 +0400

    helloWorld

---
+++ app/src/main/java/com/leblebi/gitpractice/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.leblebi.gitpractice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
